461809e364cc31c37ced0c756ba56d7f4f2af6d0
Dmitrii Palienko <dmitrii.palienko@gmail.com>
2023-02-15 08:28:33 +0200

    Settuping DB with Django ORM

--- main_website/urls.py
+++ main_website/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('docs/', views.docs, name = 'docs'),
-    path('champion/<str:slug>/', views.test, name = 'champion')
+    #path('docs/', views.docs, name = 'docs'),
+    #path('champion/<str:slug>/', views.test, name = 'champion')
 ]
